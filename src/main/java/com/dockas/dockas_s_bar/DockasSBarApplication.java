37ba9d16d66de4ecb1df29bfab8ad523a7a1e59d
carlosgiongo <carlosgiongo@users.noreply.github.com>
2022-09-21 19:41:25 -0300

    Second commit

---
+++ src/main/java/com/dockas/dockas_s_bar/DockasSBarApplication.java
@@ -0,0 +1,13 @@
+package com.dockas.dockas_s_bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockasSBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockasSBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dockas/dockas_s_bar/DockasSBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockas.dockas_s_bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockasSBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
